136fd3900fe2dc289bd52f2e683a23a7e17b00c5
Kaynec <68915270+Kaynec@users.noreply.github.com>
2021-02-10 17:10:56 +0330

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,9 @@
 var playerScore=0
 var computerScore=0
-
 function computerPlay(){
     var optiones=['rock','paper','scissors']
-    
     return optiones[Math.floor(Math.random() * optiones.length)];
 }
-
 function playRound(playerSelection,computerSelection){
 	if (playerSelection=='rock'&&computerSelection=='paper'){
         para.textContent=('You Lose Paper beats rock')
@@ -69,12 +66,8 @@ function playRound(playerSelection,computerSelection){
     }else {
         para.textContent=('it\'s a tie')
     }
-    
     return ;
 }
-
-
-
 const container = document.querySelector('#container')
 const rock = document.querySelector('#rock')
 const paper=document.querySelector('#paper')
@@ -84,37 +77,25 @@ const para2   =document.querySelector('#para2')
 const btn     = document.querySelector('#btn')
 const para3 = document.querySelector('#para3')
 
-
-
 function resetScore(){
     playerScore=0;
     computerScore=0;
     para2.textContent='playerscore 0 computerscore 0'
     para3.textContent=''
 }
-
 function game(){
-    
         rock.addEventListener('click',function(e){
             playRound("rock",computerPlay())
             para2.textContent=`YourScore ${playerScore} ComputerScore ${computerScore}`
         })
-        
         paper.addEventListener('click',function(e){
             playRound("paper",computerPlay())
             para2.textContent=`YourScore ${playerScore} ComputerScore ${computerScore}`
         })
-        
         scissors.addEventListener('click',function(e){
            playRound("scissors",computerPlay())
            para2.textContent=`YourScore ${playerScore} ComputerScore ${computerScore}`
-           
         })
         btn.addEventListener('click',resetScore)
-
-
 }
-
-
-
 game()
